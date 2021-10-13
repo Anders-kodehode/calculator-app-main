14d5c1c432e73d1f21ebbf761facfa6a42a6595f
Anders N Kodehode <anders.kodehode@gmail.com>
2021-10-13 09:09:07 +0200

    theme switch interaction in js(not working as intended)

--- index.js
+++ index.js
@@ -1,32 +1,68 @@
+let html = document.querySelector('html')
+let body = document.querySelector('body')
 let switchBtn = document.querySelector('.switch-btn')
+let resultScreen = document.querySelector('.result-screen')
+let input = document.querySelectorAll('button')
+let equalBtn = document.querySelector('.equal')
 
-let addOrRemoveClass = (element, nameClass) => {
+let addOrRemoveClass = (element, action, nameClass) => {
     action = 'add'
-        element.classList.add(nameClass)
-        element.classList.remove(nameClass)
-}
-
-let purpleMode = () => {
-    addOrRemoveClass(switchBtn, 'remove', 'slide-2')
-    addOrRemoveClass(switchBtn, 'remove', 'slide-1')
-}
-
-let lightMode = () => {
-    addOrRemoveClass(switchBtn, 'add', 'slide-2')
-    addOrRemoveClass(switchBtn, 'remove', 'slide-1')
+        ? element.classList.add(nameClass)
+        : element.classList.remove(nameClass)
 }
 
 let blueMode = () => {
-    addOrRemoveClass(switchBtn, 'add', 'slide-1')
+    addOrRemoveClass(switchBtn, 'remove', 'slide-1')
+    addOrRemoveClass(switchBtn, 'remove', 'slide-2')
+
+    html.setAttribute('color-mode', 'blue-mode')
+    addOrRemoveClass(body, 'remove', 'purple-mode')
+    addOrRemoveClass(resultScreen, 'remove', 'purple-mode')    
+
+    input.forEach(btn => {
+        addOrRemoveClass(btn, 'remove', 'purple-mode-key')
+    })
+    addOrRemoveClass(body, 'remove', 'purple-mode-body')
+    addOrRemoveClass(equalBtn, 'remove', 'purple-mode-equal')
 }
 
-let toggleTheme = () => {
-    if (switchBtn.classList.contains('slide-2')) {
-        purpleMode()
-    }   else if (switchBtn.classList.contains('slide-1')) {
+let lightMode = () => {
+    addOrRemoveClass(switchBtn, 'add', 'slide-1')
+  
+    html.setAttribute('color-mode', 'light')
+    addOrRemoveClass(body, 'add', 'light-mode')
+    addOrRemoveClass(resultScreen, 'add', 'light-mode')
+  
+    input.forEach(btn => {
+        addOrRemoveClass(btn, 'remove', 'blue-mode-key')
+    })
+    addOrRemoveClass(body, 'remove', 'blue-mode-body')
+    addOrRemoveClass(equalBtn, 'remove', 'blue-mode-equal')
+}
+
+let purpleMode = () => {
+    addOrRemoveClass(switchBtn, 'add', 'slide-2')
+    addOrRemoveClass(switchBtn, 'remove', 'slide-1' )
+  
+    html.setAttribute('color-mode', 'purple-mode')
+    input.forEach(btn => {
+        addOrRemoveClass(btn, 'add', 'purple-mode-key')
+    })
+    addOrRemoveClass(body, 'add', 'purple-mode-body')
+    addOrRemoveClass(equalBtn, 'add', 'purple-mode-equal')
+  
+    addOrRemoveClass(body, 'remove', 'light-mode')
+    addOrRemoveClass(resultScreen, 'remove', 'light-mode')
+}
+
+let toggleColorMode = () => {
+    if (switchBtn.classList.contains('slide-1')) {
         lightMode()
-    }   else if (switchBtn.classList.contains('slide-1')) {
+    } else if (switchBtn.classList.contains('slide-2')) {
+        purpleMode()
+    } else if (!switchBtn.classList.contains('')) {
         blueMode()
     }
 }
-switchBtn.addEventListener('click', toggleTheme)
+
+switchBtn.addEventListener('click', toggleColorMode)
